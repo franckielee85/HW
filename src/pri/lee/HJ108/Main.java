7f438b45bc383e6c006de3a17b72e02e675aeca7
fuyao lee <fuyao.lee@qq.com>
2022-04-12 18:07:34 +0800

    全部

--- src/pri/lee/HJ108/Main.java
+++ src/pri/lee/HJ108/Main.java
@@ -1,7 +1,10 @@
 package pri.lee.HJ108;
 
 
+
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * @author fuyao lee
@@ -18,9 +21,41 @@ public class Main {
         int result=Math.round(input);
         return result;
     }
-    public static void main(String[] args) {
+
+    /**
+     * 描述
+     * 数据表记录包含表索引index和数值value（int范围的正整数），请对表索引相同的记录进行合并，即将相同索引的数值进行求和运算，输出按照index值升序进行输出。
+     *
+     *
+     * 提示:
+     * 0 <= index <= 11111111
+     * 1 <= value <= 100000
+     *
+     * 输入描述：
+     * 先输入键值对的个数n（1 <= n <= 500）
+     * 接下来n行每行输入成对的index和value值，以空格隔开
+     *
+     * 输出描述：
+     * 输出合并后的键值对（多行）
+     */
+    public static void Function8(){
         Scanner scanner = new Scanner(System.in);
-        float input=scanner.nextFloat();
-        System.out.println(Function7(input));
+        int tableSize = scanner.nextInt();
+        Map<Integer, Integer> treeMap = new TreeMap<>();
+        for (int i = 0; i < tableSize; i++) {
+            int key = scanner.nextInt();
+            int value = scanner.nextInt();
+            if (treeMap.containsKey(key)) {
+                treeMap.put(key, treeMap.get(key) + value);
+            } else {
+                treeMap.put(key, value);
+            }
+        }
+        for (Integer key : treeMap.keySet()) {
+            System.out.println( key + " " + treeMap.get(key));
+        }
+    }
+    public static void main(String[] args) {
+
     }
 }
