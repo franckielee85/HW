769ead9720a9c6aa059abdaa253eb40ba60bc75b
Fuyao Lee <30462667+franckielee85@users.noreply.github.com>
2022-04-15 13:05:56 +0800

    Add files via upload
    17题

--- src/pri/lee/HJ108/Main.java
+++ src/pri/lee/HJ108/Main.java
@@ -84,31 +84,8 @@ public class Main {
             System.out.print(iterator.next());
         }
     }
-
-    /**
-     * 描述
-     * 编写一个函数，计算字符串中含有的不同字符的个数。字符在 ASCII 码范围内( 0~127 ，包括 0 和 127 )，换行表示结束符，不算在字符里。不在范围内的不作统计。多个相同的字符只计算一次
-     * 例如，对于字符串 abaca 而言，有 a、b、c 三种不同的字符，因此输出 3 。
-     *
-     * 数据范围： 1 \le n \le 500 \1≤n≤500
-     * 输入描述：
-     * 输入一行没有空格的字符串。
-     *
-     * 输出描述：
-     * 输出 输入字符串 中范围在(0~127，包括0和127)字符的种数。
-     *
-     */
-    public static int Function10(String input){
-        char[] charArray = input.toCharArray();
-        HashSet<Character> hashSet = new HashSet<>();
-        for (int i = 0; i < charArray.length; i++) {
-            hashSet.add(charArray[i]);
-        }
-
-        return hashSet.size();
-    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println( Function10(scanner.nextLine()));
+        Function9(scanner.nextLine());
     }
 }
